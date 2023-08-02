c78edb4816cc70405457e1fee4f480891330dd79
Mathias Micheel <49622130+buerviper@users.noreply.github.com>
2023-08-02 09:02:36 +0200

    updated descriptions
    added hashtag

--- add-descriptions.py
+++ add-descriptions.py
@@ -4,13 +4,13 @@ from pathlib import Path
 
 def generate_yaml_description(file_save, file_name):
     if file_name.split("-")[1] == "01":
-        yaml.dump({'status': 'eiyuden #EiyudenChronicle #100HeroesStrong #jrpg', 'spoiler_warning': '', 'description': 'Screenshot of Eiyuden Chronicle.',
+        yaml.dump({'status': '#eiyuden #EiyudenChronicle #100HeroesStrong #jrpg', 'spoiler_warning': '', 'description': 'Screenshot of Eiyuden Chronicle.',
                    'language': '', 'sensitivity': ''}, file_save)
     elif file_name.split("-")[1] == "rising":
-        yaml.dump({'status': 'eiyuden #EiyudenChronicleRising #100HeroesStrong #jrpg', 'spoiler_warning': '', 'description': 'Screenshot of Eiyuden Chronicle: Rising.',
+        yaml.dump({'status': '#eiyuden #EiyudenChronicleRising #100HeroesStrong #jrpg', 'spoiler_warning': '', 'description': 'Screenshot of Eiyuden Chronicle: Rising.',
                    'language': '', 'sensitivity': ''}, file_save)
     elif file_name.split("-")[1] == "kickstarter":
-        yaml.dump({'status': 'eiyuden #EiyudenChronicle #100HeroesStrong #jrpg', 'spoiler_warning': '', 'description': 'Screenshot of Eiyuden Chronicle.',
+        yaml.dump({'status': '#eiyuden #EiyudenChronicle #100HeroesStrong #jrpg', 'spoiler_warning': '', 'description': 'Screenshot of Eiyuden Chronicle.',
                    'language': '', 'sensitivity': ''}, file_save)
 
 for x in os.listdir("images"):
